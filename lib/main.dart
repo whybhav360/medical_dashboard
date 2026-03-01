7d9f6627f89d0db7f15114d7019bdfe47f463804
whybhav360 <vmmadaan51@gmail.com>
2026-03-01 13:39:25 +0530

    V1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: FirebaseAuth.instance.currentUser != null
           ? const BottomNavigation()
-          : Signin(),
+          : const Signin(),
     );
   }
 }
